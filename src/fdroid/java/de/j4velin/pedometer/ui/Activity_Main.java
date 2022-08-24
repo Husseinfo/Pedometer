eaf1af4ed5a4be61db4406639c9098b8efa37d14
Hussein Srour <husseinfo@mail.com>
2022-08-24 23:36:26 +0200

    androidX

--- src/fdroid/java/de/j4velin/pedometer/ui/Activity_Main.java
+++ src/fdroid/java/de/j4velin/pedometer/ui/Activity_Main.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2013 Thomas Hoffmann
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,11 +26,12 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.content.PermissionChecker;
 import android.text.method.LinkMovementMethod;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import androidx.core.content.PermissionChecker;
+import androidx.fragment.app.FragmentActivity;
 
 import de.j4velin.pedometer.BuildConfig;
 import de.j4velin.pedometer.R;
